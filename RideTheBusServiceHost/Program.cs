cfb0e726ad15e8023fd3b253904c48fa1725281a
Colin Manliclic <c_manliclic@fanshaweonline.ca>
2021-03-30 13:00:08 -0400

    Added ref to System.ServiceModel

--- RideTheBusServiceHost/Program.cs
+++ RideTheBusServiceHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace RideTheBusServiceHost
 {
@@ -10,6 +11,34 @@ namespace RideTheBusServiceHost
     {
         static void Main(string[] args)
         {
+            ServiceHost servHost = null;
+
+            try
+            {
+                // Register the service Address
+                //servHost = new ServiceHost(typeof(Shoe), new Uri("net.tcp://localhost:13200/CardsLibrary/"));
+
+                // Register the service Contract and Binding
+                //servHost.AddServiceEndpoint(typeof(IShoe), new NetTcpBinding(), "ShoeService");
+
+                //servHost = new ServiceHost(typeof(Shoe));
+
+
+                // Run the service
+                servHost.Open();
+
+                Console.WriteLine("Service started. Press any key to quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Wait for a keystroke
+                Console.ReadKey();
+                servHost?.Close();
+            }
         }
     }
 }
